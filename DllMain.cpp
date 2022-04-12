29189c45913074a580663a1100eafedb8364d91e
Finger <finger@spianmo.com>
2022-04-13 02:12:33 +0800

    hook tea加解密函数

--- DllMain.cpp
+++ DllMain.cpp
@@ -10,21 +10,28 @@ using namespace std;
 void __cdecl my_oi_symmetry_encrypt2(const BYTE *pInBuf, int nInBufLen, const BYTE *pKey, BYTE *pOutBuf, int *pOutBufLen){
     oi_symmetry_encrypt2(pInBuf, nInBufLen, pKey, pOutBuf, pOutBufLen);
     OutputDebugString("my_oi_symmetry_encrypt2");
+    //明文数据
+    //OutputDebugString(binaryToHex(pInBuf, nInBufLen).c_str());
+    //KEY
     //OutputDebugString(binaryToHex(pKey, 16).c_str());
-    //OutputDebugString(*pOutBufLen);
-    OutputDebugString(binaryToHex(pInBuf, nInBufLen).c_str());
+    //密文数据
+    OutputDebugString(binaryToHex(pOutBuf, *pOutBufLen).c_str());
     uint32_t encode_buf_len,decode_buf_len;
     unsigned char * encode_buf = tea_encode(pKey, pInBuf, nInBufLen, &encode_buf_len);
-    unsigned char * decode_buf = tea_decode(pKey,encode_buf,encode_buf_len,&decode_buf_len);
+    //unsigned char * decode_buf = tea_decode(pKey,encode_buf,encode_buf_len,&decode_buf_len);
     OutputDebugString(binaryToHex(encode_buf, encode_buf_len).c_str());
-    OutputDebugString(binaryToHex(decode_buf, decode_buf_len).c_str());
+    //OutputDebugString(binaryToHex(decode_buf, decode_buf_len).c_str());
 }
 
 void __cdecl my_oi_symmetry_decrypt2(const BYTE *pInBuf, int nInBufLen, const BYTE *pKey, BYTE *pOutBuf, int *pOutBufLen){
-    //OutputDebugString("my_oi_symmetry_decrypt2");
-    //OutputDebugString(binaryToHex(pKey, 16).c_str());
-    //OutputDebugString(binaryToHex(pOutBuf, nInBufLen).c_str());
     oi_symmetry_decrypt2(pInBuf, nInBufLen, pKey, pOutBuf, pOutBufLen);
+//    OutputDebugString("my_oi_symmetry_decrypt2");
+//    //密文数据
+//    OutputDebugString(binaryToHex(pInBuf, nInBufLen).c_str());
+//    //KEY
+//    OutputDebugString(binaryToHex(pKey, 16).c_str());
+//    //明文数据
+//    OutputDebugString(binaryToHex(pOutBuf, nInBufLen).c_str());
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule,
