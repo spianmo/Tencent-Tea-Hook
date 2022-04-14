93ef1567c1cc47104f3ba52419cba147023bb049
Finger <tangqianjin@healbone.xyz>
2022-04-14 15:22:45 +0800

    support mingw32 compiler

--- client.cpp
+++ client.cpp
@@ -2,19 +2,10 @@
 // Created by Administrator on 2022/4/13.
 //
 
-#include "httplib.h"
 #include <iostream>
 
-#define HOST "localhost:8787"
 using namespace std;
-using namespace httplib;
-
 int main() {
-//    Client cli(HOST);
-//    cli.Post("/obtain", "66666", "text/plain");
     system("chcp 65001");
-    stringstream ss;
-    ss << "==================客户端发包==================" << endl;
-    cout << ss.str() << endl;
     return 0;
 }
